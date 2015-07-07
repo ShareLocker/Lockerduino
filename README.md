ddef13acb30ebe8308c98158a1f81258a9281c66
Alan R <alan.rominger@gmail.com>
2015-07-07 18:29:23 -0400

    have example client connecting to Django

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Lockerduino
 Arduino scripts for the motor based lockers
+
+http://bildr.org/2011/06/arduino-ethernet-pin-control/
+
+# Connection Test
+
+WebClient_Test works as of Tuesday, July 7, 2015. It uses an IP on a local
+network not connected to the internet. To get one's IP address use
+ifconfig, and it will be somewhere in the output.
+
+To run the server and make it available on the network
+
+    python manage.py runserver {IP}:8000
+
+Where {IP} is the IP address from the last step.
---
+++ relay_demo/relay_demo.ino
@@ -0,0 +1,47 @@
+
+int row;
+int col;
+
+void setup()
+{
+    for (int thisPin = 2; thisPin < 8; thisPin++)  {
+      pinMode(thisPin, OUTPUT);      
+    }
+    for (int thisPin = 2; thisPin < 8; thisPin++)  {
+      digitalWrite(thisPin, HIGH);   
+    }
+    row = 1;
+    col = 1;
+}
+
+void loop()
+{
+  
+  ElementOn(row, col);
+  delay(1000);
+  ElementOff(row, col);
+  Serial.print("row ");
+  Serial.print(row);
+  Serial.print(" col ");
+  Serial.println(col);
+  col = col + 1;
+  if (col > 3) {
+    col = 1;
+    row = row + 1;
+    if (row > 3) {
+      row = 1;
+    }
+  }
+}
+
+void ElementOn(int row, int col) {
+    for (int thisPin = 2; thisPin < 8; thisPin++)  {
+      digitalWrite(thisPin, HIGH);
+    }
+    digitalWrite(1+row, LOW);
+    digitalWrite(4+col, LOW);
+}
+void ElementOff(int row, int col) {
+    digitalWrite(1+row, HIGH);
+    digitalWrite(4+col, HIGH);
+}
